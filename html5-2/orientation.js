a10cb95baa1bba716fb4cccea9846b06561e0b55
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-05 13:41:14 +0200

    rot

--- html5-2/orientation.js
+++ html5-2/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.2";
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
 
@@ -32,5 +32,8 @@ function on_device_motion(e)
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+	
+	document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x / acc_z) * 100) / 100;
+	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y / acc_z) * 100) / 100;
 }
 //--------------------------------
