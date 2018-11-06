10dbd349fb25eb2f0990f6da3623d5b0cf16e497
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-06 18:40:55 +0200

    Update orientation.js

--- seia/orientation.js
+++ seia/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.06.0";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
@@ -11,7 +11,11 @@ function la_schimbare_gyro(e)
   var canvas = document.getElementById("id_canvas");
   var context = canvas.getContext("2d");
   
-   context.beginPath();
-  context.arc(canvas.width / 2, canvas.height / 2, 30, 0, 2 * Math.PI);
+  var R = 10;
+  
+  context.beginPath();
+  context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R), 
+              canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), 
+              R, 0, 2 * Math.PI);
   context.stroke();
 }
