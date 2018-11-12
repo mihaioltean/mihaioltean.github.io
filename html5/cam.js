bf09c132fde3691d9808fc3c5d4ffa93f22fcee0
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-12 11:16:09 +0200

    environment

--- html5/cam.js
+++ html5/cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.0";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1";
 document.getElementById("id_button").addEventListener("click", start);
 
 //------------------------------
@@ -14,7 +14,7 @@ function on_failure(e)
 //------------------------------
 function start()
 {
-	var c = {audio: true, video: true};
+	var c = {audio: true, video: {facingMode: "environment"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
 //------------------------------
