ed0ad9f67aa7c15ab1ec48d91ad9f060d0ae99bd
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-12 14:41:40 +0200

    video

--- html5-2/cam.js
+++ html5-2/cam.js
@@ -1,6 +1,7 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.12.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.12.2";
 document.getElementById("id_button").addEventListener("click", start);
-
+document.getElementById("id_canvas").addEventListener("touchstart", on_touch);
+document.getElementById("id_canvas").addEventListener("mousedown", on_touch);
 
 //-------------------------------
 function on_success(e)
@@ -17,5 +18,13 @@ function start()
 {
 	var c = {audio: true, video: {facingMode:"environment"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
+}
+//-------------------------------
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var video = document.getElementById("id_video");
+	context.drawImage(video, 0, 0);
 }
 //-------------------------------
