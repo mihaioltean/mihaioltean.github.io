61eef7778ee7d774cd592faf13580c67071cf73a
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-12 14:18:20 +0200

    cam

---
+++ html5-2/cam.js
@@ -0,0 +1,21 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.12.0";
+document.getElementById("id_button").addEventListener("click", start);
+
+
+//-------------------------------
+function on_success(e)
+{
+	document.getElementById("id_video").srcObject = e;
+}
+//-------------------------------
+function on_failure(e)
+{
+	alert("Cannot read camera");
+}
+//-------------------------------
+function start()
+{
+	var c = {audio: true, video = true};
+	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
+}
+//-------------------------------
