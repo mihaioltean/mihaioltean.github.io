d52c0ef006ab1042f46af0b3837e57291ae36ca8
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-12 11:35:59 +0200

    snap

--- html5/cam.js
+++ html5/cam.js
@@ -1,5 +1,7 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.2";
 document.getElementById("id_button").addEventListener("click", start);
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
 //------------------------------
 function on_success(e)
@@ -14,7 +16,15 @@ function on_failure(e)
 //------------------------------
 function start()
 {
-	var c = {audio: true, video: {facingMode: "environment"}};
+	var c = {audio: true, video: {facingMode: "user"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
 //------------------------------
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var video = document.getElementById("id_video");
+	
+	context.drawImage(video, 0, 0);
+}
