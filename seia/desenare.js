ac3a285f635bccbf9c085c3536ff9e90db74d47a
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-20 18:46:36 +0200

    Add files via upload

--- seia/desenare.js
+++ seia/desenare.js
@@ -1,19 +1,37 @@
 document.getElementById("id_logic_version").innerHTML = 
-		"Logic version = 2018.11.20.1";
+		"Logic version = 2018.11.20.3";
 		
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchmove", on_touch_move);
 
+var rect = canvas.getBoundingClientRect();
+//---------------------------------
 function on_touch(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches.item(i).pageX,
-					e.changedTouches.item(i).pageY,
+		context.arc(e.changedTouches.item(i).pageX - rect.left,
+					e.changedTouches.item(i).pageY - rect.top,
 					20,
 					0, 2 * Math.PI
 					);
 		context.stroke();			
 	}
 }
+//---------------------------------
+function on_touch_move(e)
+{
+	for (var i = 0; i < e.changedTouches.length; i++){
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouches.item(i).pageX - rect.left,
+					e.changedTouches.item(i).pageY - rect.top,
+					20,
+					0, 2 * Math.PI
+					);
+		context.stroke();			
+	}	
+}
+//---------------------------------
