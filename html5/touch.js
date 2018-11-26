b6766e12a5d71087b4f0ba3890cacefc2899dd49
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-26 11:47:27 +0200

    line width

--- html5/touch.js
+++ html5/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.5";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.6";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -35,10 +35,12 @@ function on_touch_move(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.lineWidth = 20;
 		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
 		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
 						e.changedTouches[i].pageY - canvas_bounding_rect.top);
 		
+		context.lineWidth = 1;
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
 					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
