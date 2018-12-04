22d4e945acb413e31a41a84cf168baaa01517d3b
Mihai Oltean <mihai.oltean@gmail.com>
2018-12-04 18:08:28 +0200

    Add files via upload

--- seia/synth.js
+++ seia/synth.js
@@ -1,6 +1,7 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.3";
 var synth = window.speechSynthesis;
-	
+
+//----------------------------------
 function afiseaza_voci()
 {
 	var lista_voci = synth.getVoices();
@@ -9,3 +10,12 @@ function afiseaza_voci()
 		document.getElementById("id_voci").innerHTML +=
 				lista_voci[i].lang + " " + lista_voci[i].name + "<br>";
 }
+//-----------------------------------
+function vorbeste()
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.text = document.getElementById("id_text").value;
+	enunt.lang = "en-US";
+	synth.speak(enunt);
+}
+//----------------------------------
