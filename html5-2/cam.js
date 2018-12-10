251315441a51371740dce4b6fded944348251346
Mihai Oltean <mihai.oltean@gmail.com>
2018-12-10 12:43:50 +0200

    updated

--- html5-2/cam.js
+++ html5-2/cam.js
@@ -1,7 +1,7 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.12.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.12.3";
 document.getElementById("id_button").addEventListener("click", start);
-document.getElementById("id_canvas").addEventListener("touchstart", on_touch);
-document.getElementById("id_canvas").addEventListener("mousedown", on_touch);
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
 //-------------------------------
 function on_success(e)
