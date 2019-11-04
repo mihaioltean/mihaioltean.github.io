0fe8b872c2e3764b59799a7d79ff176c451fc4ce
Mihai Oltean <mihai.oltean@gmail.com>
2019-11-04 14:48:08 +0200

    Add files via upload

--- html2/gyro/gyro.js
+++ html2/gyro/gyro.js
@@ -1,6 +1,8 @@
-document.getElementById("id_logic").innerHTML = "Logic = 2019.11.04.1";
+document.getElementById("id_logic").innerHTML = "Logic = 2019.11.04.2";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
+window.addEventListener("devicemotion", on_acc_data_uab);
+
 
 function on_gyro_data_uab(e)
 {
@@ -9,3 +11,11 @@ function on_gyro_data_uab(e)
 	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma *100)/ 100;
 }
 
+function on_acc_data_uab(e)
+{
+	var acc = e.accelerationIncludingGravity;
+	
+	document.getElementById("id_acc_x").innerHTML = Math.round(acc.x * 100)/100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(acc.y * 100)/100;
+	document.getElementById("id_acc_z").innerHTML = Math.round(acc.z * 100)/100;
+}
