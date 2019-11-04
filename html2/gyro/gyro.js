b49bfdc825c331c8e568560915bc4b0c63bfb70f
Mihai Oltean <mihai.oltean@gmail.com>
2019-11-04 15:51:37 +0200

    Add files via upload

--- html2/gyro/gyro.js
+++ html2/gyro/gyro.js
@@ -3,12 +3,32 @@ document.getElementById("id_logic").innerHTML = "Logic = 2019.11.04.2";
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
 
+function desenare(unghi_x, unghi_y)
+{
+	// obtinem referinta la canvas
+	var canvas = document.getElementById("id_canvas");
+	// obtinem referinta la context
+	var context = canvas.getContext("2d");
+	// stergem canvasul
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	// incepem sa construim o cale (path)
+	context.beginPath();
+	var r = 10;
+	var x = canvas.width / 2 + unghi_x / 90 * (canvas.width / 2 - r);
+	var y = canvas.height / 2 + unghi_y / 90 * (canvas.height / 2 - r);
+	// adaugam cercul la cale
+	context.arc(x, y, r, 0, 2 * Math.PI);
+	// desenam calea
+	context.stroke();
+}
 
 function on_gyro_data_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha *100)/ 100;
 	document.getElementById("id_beta").innerHTML = Math.round(e.beta *100)/ 100;
 	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma *100)/ 100;
+	
+	desenare(e.beta, e.gamma);
 }
 
 function on_acc_data_uab(e)
