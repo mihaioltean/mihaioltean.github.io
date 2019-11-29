a55dbb24d8e7c5e32f3c4cb24c4ef61461bb42d6
Mihai Oltean <mihai.oltean@gmail.com>
2019-11-29 14:36:16 +0200

    Add files via upload

--- html3/touch/touch.js
+++ html3/touch/touch.js
@@ -1,7 +1,9 @@
-document.getElementById("id_logic").innerHTML = "2019.11.29.6";
+document.getElementById("id_logic").innerHTML = "2019.11.29.7";
 
 window.addEventListener("touchstart", touch_start_uab, { passive: false }); 
 window.addEventListener("touchmove", touch_move_uab, { passive: false }); 
+window.addEventListener("touchend", touch_end_uab, { passive: false }); 
+
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -67,5 +69,21 @@ function touch_move_uab(e)
 		
 		last_touch[touch_index].x = t[i].x;
 		last_touch[touch_index].y = t[i].y;
+	}
+}
+
+function touch_end_uab(e)
+{
+	e.preventDefault();
+
+	var t = e.changedTouches;
+	for (var i = 0; i < t.length; i++){
+		var touch_index = -1;
+		for (var j = 0; j < last_touch.length; j++)
+			if (t[i].identifier == last_touch[j].id){
+				touch_index = j;
+				break;
+			}
+		last_touch.splice(touch_index, 1);
 	}
 }
