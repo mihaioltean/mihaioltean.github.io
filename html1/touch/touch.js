8d6fa6e4772c86021ce54a376341671698071926
Mihai Oltean <mihai.oltean@gmail.com>
2019-12-02 12:32:42 +0200

    Add files via upload

--- html1/touch/touch.js
+++ html1/touch/touch.js
@@ -1,7 +1,7 @@
-document.getElementById("id_logic").innerHTML = "Logic: 2019.12.02.3";
+document.getElementById("id_logic").innerHTML = "Logic: 2019.12.02.4";
 
 window.addEventListener("touchstart", touch_start_uab); 
-window.addEventListener("touchmove", touch_move_uab); 
+window.addEventListener("touchmove", touch_move_uab, {passive:false}); 
 window.addEventListener("touchend", touch_end_uab);
 
 var canvas = document.getElementById("id_canvas");
@@ -44,6 +44,8 @@ function touch_start_uab(p)
 
 function touch_move_uab(p)
 {
+	p.preventDefault();
+	
 	var t = p.changedTouches;
 	for (var i = 0; i < t.length; i++){
 		var index_t = -1;
